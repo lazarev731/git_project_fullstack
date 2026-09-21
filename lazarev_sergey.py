78eec304bd0df42867e44b5c68f32e899a182cff
Levysz <lazarevsereza20@gmail.com>
2026-09-21 16:25:05 +0500

    создание файла с данными студента

---
+++ lazarev_sergey.py
@@ -0,0 +1,8 @@
+def main():
+    print("Лазарев Сергей")
+    print("Группа ИВТ-ИСУ-203Б, подгруппа 1")
+    print("21.09.2026")
+    print("Show must go on")
+
+if __name__ == '__main__':
+    main()
